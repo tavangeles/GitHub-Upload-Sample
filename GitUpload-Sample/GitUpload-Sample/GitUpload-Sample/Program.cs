12b64330cd44956904b11a74f97d938fbeabd3b1
Juries <markjuriesmabiling@gmail.com>
2019-05-15 12:29:55 +0800

    test 2

--- GitUpload-Sample/GitUpload-Sample/GitUpload-Sample/Program.cs
+++ GitUpload-Sample/GitUpload-Sample/GitUpload-Sample/Program.cs
@@ -11,7 +11,7 @@ namespace GitUpload_Sample
         static void Main(string[] args)
         {
             Console.WriteLine("Sample Git Upload");
-            Console.WriteLine("test");
+            Console.WriteLine("test2");
             Console.ReadLine();
         }
     }
