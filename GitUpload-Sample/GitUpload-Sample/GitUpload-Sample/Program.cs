18e507917bb7c6d4193536ef4ccac9c868a5aa42
Juries <markjuriesmabiling@gmail.com>
2019-05-15 15:23:46 +0800

    test 3

--- GitUpload-Sample/GitUpload-Sample/GitUpload-Sample/Program.cs
+++ GitUpload-Sample/GitUpload-Sample/GitUpload-Sample/Program.cs
@@ -12,6 +12,7 @@ namespace GitUpload_Sample
         {
             Console.WriteLine("Sample Git Upload");
             Console.WriteLine("Sample Git Upload2");
+            Console.WriteLine("Sample Git Upload3");
             Console.ReadLine();
         }
     }
